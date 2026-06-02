095c8b9172a0dbfc09bf276c7812a00c7d05a4ce
zhangping <136666258@qq.com>
2026-06-02 19:54:19 +0800

    vault backup: 2026-06-02 19:54:19

--- Python语法2025/python_code/07文件操作/02_for循环读取文件.py
+++ Python语法2025/python_code/07文件操作/02_for循环读取文件.py
@@ -1,20 +1,18 @@
-
-
 # 1. 打开 2 读取 3关闭
 # 方式1
-f = open("D:/hi.txt", "r", encoding="utf-8")
+# f = open("D:/hi.txt", "r", encoding="utf-8")
+f = open("E:\download\技术栈-私\API-key.md", "r", encoding="utf-8")
 
 for line in f:
-    print(line.strip())     # 还是需要自行处理\n
+    print(line.strip())  # 还是需要自行处理\n
 
-#这个写法等同于下面的写法
+# 这个写法等同于下面的写法
 # for line in f.readlines():
 #     print(line.strip())
 f.close()
 
-
 # 方式2
-for line in open("D:/hi.txt", "r", encoding="utf-8"):
+for line in open("E:\download\技术栈-私\API-key.md", "r", encoding="utf-8"):
     print(line.strip())
 # 这个方式3个步骤都集成了
 # open是打开文件，  for循环读取行   for循环结束后会自动close
