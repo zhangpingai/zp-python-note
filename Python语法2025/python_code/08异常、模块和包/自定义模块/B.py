236ce1524ab468a5a736a2f7626f0d253781ee42
zhangping <136666258@qq.com>
2026-06-02 20:50:30 +0800

    vault backup: 2026-06-02 20:50:30

--- Python语法2025/python_code/08异常、模块和包/自定义模块/B.py
+++ Python语法2025/python_code/08异常、模块和包/自定义模块/B.py
@@ -1,5 +1,3 @@
-
-
 def say_hi():
     print('Hello World!')
 
@@ -16,11 +14,10 @@ name = "张三丰"
 age = 11
 height = 172.56
 
-
 # 如果本文件被直接执行，则内置变量 __name__ 会被赋值为："__main__"
 # 如果本文件被import或from导入，则内置变量__name__会被赋值为：文件名称
 # __name__是Python内置变量，任何代码文件都有
 # 我们可以通过读取这个变量的值，从而确定这个代码文件是：被人导入了 还是被作为程序执行了
+print(f'__name__ = {__name__}')
 if __name__ == '__main__':
     wangwang()
-
